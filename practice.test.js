cad1e33d1398cfd3e992ce6f1700af00cf0cbcb2
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-10-23 20:00:30 +0530

    PHASE 1

--- practice.test.js
+++
@@ -1,9 +0,0 @@
-const rewire = require("rewire")
-const practice = rewire("./practice")
-const put_data = practice.__get__("put_data")
-// @ponicode
-describe("put_data", () => {
-    test("0", async () => {
-        await put_data()
-    })
-})
